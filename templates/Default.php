3aabeaad2bcbe9721602d45ff4af028fab6087f0
Zcytxcbyz <zcytxcbyz@163.com>
2020-07-09 20:59:42 +0800

    Update

--- templates/Default.php
+++ templates/Default.php
@@ -8,7 +8,7 @@ $WebsiteName='Zcytxcbyz';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/Default.css">
-    <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
